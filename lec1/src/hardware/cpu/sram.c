b63fba1e6ac12320c1652df307df267f43f194c2
M <bicc1993@163.com>
2024-06-17 22:39:24 +0800

    # cache write init

--- lec1/src/hardware/cpu/sram.c
+++ lec1/src/hardware/cpu/sram.c
@@ -90,4 +90,63 @@ uint8_t sram_cache_read(uint64_t address) {
 }
 
 
+void sram_cache_write(uint64_t address, uint8_t data) {
+    address_t paddr = {
+            .paddr_value = address,
+    };
+    sram_cacheset_t cache_set = cache.sets[paddr.CI];
+    sram_cacheline_t *invalid = NULL;
+    sram_cacheline_t *victim = NULL;
+    int max_time = -1;
+    for (int i = 0; i < NUM_CACHE_LINE_PER_SET; i++) {
+
+        sram_cacheline_t *line = &cache_set.lines[i];
+        line->time += 1;
+
+        if (max_time < line->time) {
+            max_time = line->time;
+            victim = line;
+        }
+        if (line->state == CACHE_LINE_INVALID) {
+            invalid = line;
+        }
+
+    }
+
+    for (int i = 0; i < NUM_CACHE_LINE_PER_SET; i++) {
+
+        sram_cacheline_t *line = &cache_set.lines[i];
+
+        if (CACHE_LINE_INVALID != line->state && line->tag == paddr.CT) {
+            line->time = 0;
+            line->block[paddr.CO] = data;
+            line->state = CACHE_LINE_DIRTY;
+            return;
+        }
+
+    }
+    if (invalid != NULL) {
+        read_bus_cacheline(paddr.address_value, &(invalid->block));
+        invalid->state = CACHE_LINE_DIRTY;
+        invalid->tag = paddr.CT;
+        invalid->time = 0;
+        invalid->block[paddr.CO] = data;
+        return;
+    }
+
+    assert(victim != NULL);
+    if (victim->state == CACHE_LINE_DIRTY) {
+        write_bus_cacheline(paddr.address_value, &(victim->block));
+    }
+    victim->state = CACHE_LINE_INVALID;
+    read_bus_cacheline(paddr.address_value, &(victim->block));
+    victim->state = CACHE_LINE_CLEAN;
+    victim->time = 0;
+    victim->tag = paddr.CT;
+    victim->block[paddr.CO] = data;
+
+}
+
+
+
 
